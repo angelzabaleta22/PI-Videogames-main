67056d028a08217307f4d900eac8be5520d4e3b3
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2022-12-06 17:31:20 -0500

    Cambio de nombre

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,27 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    //column1
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    img: {
+      type: DataTypes.STRING,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+    },
+    Rating: {
+      type: DataTypes.DECIMAL,
     },
   });
 };
